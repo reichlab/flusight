bcdc8c98bc71726c072dac8d7b47950733a19ed1
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-10-06 02:48:08 -0400

    Add template for parse npm-script

---
+++ build/parse.js
@@ -0,0 +1,14 @@
+// Convert data files for ingestion
+//
+// Specifically, tranform ../data and ../config.yaml
+// and create static assets in ../src/assets for ingestion
+
+const path = require('path')
+const ora = require('ora')
+
+var spinner = ora("Reading files...")
+spinner.start()
+
+console.log("Not implemented")
+
+spinner.stop()
