2fc30893063152e528325e1b58fcb5607f234491
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-10-14 04:58:05 -0400

    config reader

---
+++ parse/modules/config.js
@@ -0,0 +1,12 @@
+// Modules for reading config.yaml file
+
+const yaml = require('js-yaml')
+const fs = require('fs')
+
+/**
+ * Return object read from config file
+ * @param {string} config file
+ */
+const read = (configFile) => {
+  return yaml.safeLoad(fs.readFileSync(configFile, 'utf8'))
+}
