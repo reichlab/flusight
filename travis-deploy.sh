4b6942f787323227ddb7dfdb26862f74dddb0e9d
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-11-06 14:11:26 -0500

    Travis auto deploy scripts

---
+++ travis-deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    npm run build
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Run deployment steps
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+rm -rf ./dist/* || exit 0
+
+npm run build
+cp -r ./dist/* ./
+
+git config user.name "Travis CI Auto Deploy"
+git config user.email "abhinav.tushar.vs@gmail.com"
+
+git add .
+git commit -m "Auto deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_private.enc -out deploy_private -d
+chmod 600 deploy_private
+eval `ssh-agent -s`
+ssh-add deploy_private
+
+# Push to gh-pages
+git push $SSH_REPO $TARGET_BRANCH --force
