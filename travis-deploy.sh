2d895739cd68157733f00afe52417f1b44089217
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-11-06 14:37:58 -0500

    Track remote gh-pages

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
-
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
@@ -17,7 +14,7 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 # Run deployment steps
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git checkout -b gh-pages origin/gh-pages
 rm -rf ./dist/* || exit 0
 
 npm run build
@@ -40,4 +37,4 @@ eval `ssh-agent -s`
 ssh-add deploy_private
 
 # Push to gh-pages
-git push $SSH_REPO $TARGET_BRANCH --force
+git push $SSH_REPO gh-pages --force
