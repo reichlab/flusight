e515b27b63f8e42c4bb01f469080f3ec23f8e834
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-11-06 14:41:39 -0500

    Orphan env variable reference

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,7 @@
 set -e # Exit with nonzero exit code if anything fails
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
     echo "Skipping deploy; just doing a build."
     npm run build
     exit 0
