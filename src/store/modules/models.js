f187adc1b3a40348e238025347f905a369eab62b
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-12-30 00:19:29 -0500

    Show log score by default and highlight highest one

--- src/store/modules/models.js
+++ src/store/modules/models.js
@@ -3,15 +3,17 @@ import * as d3 from 'd3'
 
 const state = {
   colors: d3.schemeCategory10,
-  selectedStat: 0,
+  selectedStat: 1,
   stats: [{
     id: 'mae',
     name: 'Mean Absolute Error',
-    url: 'https://en.wikipedia.org/wiki/Mean_absolute_error'
+    url: 'https://en.wikipedia.org/wiki/Mean_absolute_error',
+    bestFunc: Math.min
   }, {
     id: 'log',
     name: 'Log Score',
-    url: '#'
+    url: 'https://en.wikipedia.org/wiki/Scoring_rule#Logarithmic_scoring_rule',
+    bestFunc: Math.max
   }]
 }
 
@@ -62,14 +64,14 @@ const getters = {
         return ob
       })
 
-      // Apply properties to lowest error item
-      // Don't go for it when value if null
+      // Apply properties to best item
+      // Don't go for it when value is null
       if (data[0]['oneWk']) {
         keys.forEach(key => {
           let perKey = data.map(d => d[key])
-          let minIdx = perKey.indexOf(Math.min(...perKey))
+          let bestIdx = perKey.indexOf(statsMeta.bestFunc(...perKey))
 
-          output.data[minIdx][key].best = true
+          output.data[bestIdx][key].best = true
         })
       }
 
