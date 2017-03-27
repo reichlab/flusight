8b7191def7921c95e7c8e69e8ffa34a6ff4d5502
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-03-27 04:14:22 -0400

    Move display options in flusight

--- src/components/Panels/TimeChart.vue
+++ src/components/Panels/TimeChart.vue
@@ -23,8 +23,33 @@ export default {
     ])
   },
   ready () {
+
+    let timeChartOptions = {
+      baseline: {
+        text: ['CDC', 'Baseline'],
+        description: `Baseline ILI value as defined by CDC.
+                      <br><br><em>Click to know more</em>`,
+        url: 'http://www.cdc.gov/flu/weekly/overview.htm'
+      },
+      axes: {
+        x: {
+          title: ['Epidemic', 'Week'],
+          description: `Week of the calendar year, as measured by the CDC.
+                        <br><br><em>Click to know more</em>`,
+          url: 'https://wwwn.cdc.gov/nndss/document/MMWR_Week_overview.pdf'
+        },
+        y: {
+          title: 'Weighted ILI (%)',
+          description: `Percentage of outpatient doctor visits for
+                        influenza-like illness, weighted by state population.
+                        <br><br><em>Click to know more</em>`,
+          url: 'http://www.cdc.gov/flu/weekly/overview.htm'
+        }
+      }
+    }
+
     // Initialize time chart
-    let timechart = new TimeChart('#timechart')
+    let timechart = new TimeChart('#timechart', timeChartOptions)
 
     timechart.eventHooks.push(eventData => {
       if (eventData.type === 'positionUpdate') {
