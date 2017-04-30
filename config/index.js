c972b04845750b8f13edf291d5e25db323e4692b
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-04-30 18:08:19 -0400

    Port change to 8810

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8888,
+    port: 8810,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
