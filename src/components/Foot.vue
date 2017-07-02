b93b6cbabc7ab9a45720e484ff8f3940aa384b11
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-07-02 03:12:37 -0400

    Add disclaimer at the bottom

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -13,6 +13,20 @@
 </style>
 
 <template lang="pug">
+.modal#disclaimer-modal
+  .modal-background
+  .modal-card
+    header.modal-card-head
+      p.modal-card-title Disclaimer
+      button.delete
+    section.modal-card-body
+      | This work has been supported by the Defense Advanced Research Projects Agency
+      | Young Faculty Award (#D16AP00144) and by the National Institutes of General
+      | Medical Sciences (R35GM119582). The content is solely the responsibility of
+      | the authors and does not necessarily represent the official views of the DARPA,
+      | the United States Department of Defense, NIGMS, or the National Institutes of Health.
+    footer.modal-card-foot
+
 footer.footer
   .container
     .content.has-text-centered
@@ -23,6 +37,8 @@ footer.footer
       | <a href="https://github.com/d3/d3/wiki#supported-environments">here</a>.
       | The <a v-bind:href="branding.sourceUrl">source</a> is licensed
       | <a href="https://opensource.org/licenses/MIT">MIT</a>.
+      br
+      | <a id="disclaimer-btn" href="#">Funding and disclaimer</a>
 </template>
 
 <script>
@@ -31,6 +47,25 @@ import { mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapGetters(['branding', 'updateTime'])
+  },
+  ready () {
+    let showModal = () => {
+      document.getElementById('disclaimer-modal').className = 'modal is-active'
+    }
+
+    let hideModal = () => {
+      document.getElementById('disclaimer-modal').className = 'modal'
+    }
+
+    document.getElementById('disclaimer-btn').addEventListener('click', showModal)
+    document.querySelector('#disclaimer-modal .modal-background').addEventListener('click', hideModal)
+    document.querySelector('#disclaimer-modal button.delete').addEventListener('click', hideModal)
+
+    window.addEventListener('keyup', evt => {
+      if (evt.which === 27 || evt.keyCode === 27) {
+        hideModal()
+      }
+    })
   }
 }
 </script>
