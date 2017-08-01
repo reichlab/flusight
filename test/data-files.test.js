525c80a09b9273c4d128a68d371097072a683235
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-01 23:05:50 +0530

    New tests

---
+++ test/data-files.test.js
@@ -0,0 +1,35 @@
+/**
+ * Test the expected data files in ./src/assets/data
+ */
+
+/* global describe it */
+const chai = require('chai')
+const path = require('path')
+const fs = require('fs')
+
+chai.should()
+
+describe('History file', function () {
+  it('should be generated', function () {
+    fs.existsSync('./src/assets/data/history.json').should.be.true
+  })
+})
+
+describe('Metadata file', function () {
+  it('should be generated', function () {
+    fs.existsSync('./src/assets/data/metadata.json').should.be.true
+  })
+})
+
+describe('Season data files', function () {
+  it('should be generated', function () {
+    // Get a list of seasons from the ./data directory
+    let seasons = fs.readdirSync('./data').filter(file => {
+      return fs.statSync(path.join('./data', file)).isDirectory()
+    })
+
+    seasons.forEach(season => {
+      fs.existsSync(`./src/assets/data/season-${season}.json`).should.be.true
+    })
+  })
+})
