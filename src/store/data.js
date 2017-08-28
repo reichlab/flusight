598fd682efc359e5e8c5256876ddea9047cd1f0c
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-28 17:39:24 -0400

    Load distributions directory using file-loader

--- src/store/data.js
+++ src/store/data.js
@@ -1,18 +1,35 @@
 import history from '!json!../assets/data/history.json'
 import metadata from '!json!../assets/data/metadata.json'
+// Loading latest season in the main bundle itself
 import latestSeasonData from '!json!../assets/data/season-latest.json'
 
-const req = require.context(
+const seasonDataCtx = require.context(
   'file-loader!../assets/data/',
   false,
   /^\.\/.*\.json$/
 )
 
-const seasonDataUrls = req.keys().reduce((acc, key) => {
-  if (key.startsWith('./season-2')) {
+const distDataCtx = require.context(
+  'file-loader!../assets/data/distributions/',
+  false,
+  /^\.\/.*\.json$/
+)
+
+const seasonDataUrls = seasonDataCtx.keys().reduce((acc, key) => {
+  if (key.startsWith('./season-')) {
+    // Identifier is like '2013-2014'
     acc[key.slice(9, -5)] = req(key)
   }
   return acc
 }, {})
 
-export { seasonDataUrls, latestSeasonData, history, metadata }
+const distDataUrls = distDataCtx.keys().reduce((acc, key) => {
+  if (key.startsWith('./season-')) {
+    // Identifier is like '2013-2014-hhs10'
+    acc[key.slice(9, -5)] = req(key)
+  }
+  return acc
+}, {})
+
+
+export { seasonDataUrls, distDataUrls, latestSeasonData, history, metadata }
