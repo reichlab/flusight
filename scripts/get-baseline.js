cb57b504b385744f9062e4b9b0f78ac83f57a99e
Abhinav Tushar <lepisma@fastmail.com>
2017-12-25 17:52:18 -0500

    Download baseline using fct

--- scripts/get-baseline.js
+++ scripts/get-baseline.js
@@ -2,13 +2,11 @@
  * Download and save baseline file
  */
 
-const fs = require('fs')
-const download = require('download')
-
-const config = require('./utils').readYaml('./config.yaml')
-
-let baselineUrl = config.scripts.baselineUrl
+const fct = require('flusight-csv-tools')
 let baselineFile = './scripts/assets/wILI_Baseline.csv'
 
-console.log('Downloading baseline file')
-download(baselineUrl).pipe(fs.createWriteStream(baselineFile))
+fct.truth.getBaselineData(baselineFile).then(d => {
+  console.log('File dowloaded')
+}).catch(e => {
+  console.log(e)
+})
