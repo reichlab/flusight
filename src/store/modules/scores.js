03e281deb1e494757eeecb15c949bc867f115d6c
Abhinav Tushar <lepisma@fastmail.com>
2018-01-31 00:59:36 -0500

    Open score description in new tab

--- src/store/modules/scores.js
+++ src/store/modules/scores.js
@@ -24,7 +24,7 @@ const state = {
     name: 'Mean Absolute Error',
     url: 'https://github.com/reichlab/flusight/wiki/Scoring#1-absolute-error',
     bestFunc: items => Math.min(...items.filter(d => d !== null)),
-    desc: `<a href='https://github.com/reichlab/flusight/wiki/Scoring#1-absolute-error'>
+    desc: `<a href='https://github.com/reichlab/flusight/wiki/Scoring#1-absolute-error' target='_blank'>
              Absolute error
            </a> is the absolute value of difference between the eventually
            observe value and point prediction.`
@@ -33,7 +33,7 @@ const state = {
     name: 'Mean Log Score (single bin)',
     url: 'https://github.com/reichlab/flusight/wiki/Scoring#2-log-score-single-bin',
     bestFunc: items => Math.max(...items.filter(d => d !== null)),
-    desc: `<a href='https://github.com/reichlab/flusight/wiki/Scoring#2-log-score-single-bin'>
+    desc: `<a href='https://github.com/reichlab/flusight/wiki/Scoring#2-log-score-single-bin' target='_blank'>
              Single bin log-scores
            </a> are computed by taking natural log of predicted probability
            for the eventually observed value.`
@@ -42,7 +42,7 @@ const state = {
     name: 'Mean Log Score (multi bin)',
     url: 'https://github.com/reichlab/flusight/wiki/Scoring#3-log-score-multi-bin',
     bestFunc: items => Math.max(...items.filter(d => d !== null)),
-    desc: `<a href='https://github.com/reichlab/flusight/wiki/Scoring#3-log-score-multi-bin'>
+    desc: `<a href='https://github.com/reichlab/flusight/wiki/Scoring#3-log-score-multi-bin' target='_blank'>
              Multi bin log-scores
            </a> are computed by summing the predicted probabilities around
            a window of the eventually observed values. For the k-week-ahead
