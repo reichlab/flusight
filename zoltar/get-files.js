778280705c2940a57923b44589cb37b9bd8f9cab
Abhinav Tushar <lepisma@fastmail.com>
2018-03-07 01:30:21 -0500

    Ignore zoltar in standard

--- zoltar/get-files.js
+++ zoltar/get-files.js
@@ -1,5 +1,34 @@
 // Script to download files from zoltar
 
-const fs = require('fs')
+const fs = require('fs-extra')
+const moment = require('moment')
 const mmwr = require('mmwr-week')
 const zoltar = require('./zoltar')
+
+// Config
+const config = require('./config')
+
+async function fileExists (filePath) {
+  return fs.pathExists(filePath)
+}
+
+async function downloadFile (url, outputFile) {
+  // TODO
+}
+
+function fileNameTransformer (zolterFile) {
+  // TODO Parse the date thing
+  let parsedDate
+  let mdate = new mmwr.MMWRDate(0, 0)
+  mdate.fromMomentDate(moment(parsedData))
+
+  // TODO
+  return localFile
+}
+
+let z = zoltar.Zoltar(config['zoltar-root'])
+let project = z.getProject(config['project'])
+
+let models = project.getModels()
+
+// For each model, check if we have files
