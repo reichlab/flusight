b23ebc086aeda918f8dcfe1ac0f08051be18acdc
Abhinav Tushar <lepisma@fastmail.com>
2018-03-06 19:46:54 -0500

    Skeleton for zoltar scripts

---
+++ zoltar/get-files.js
@@ -0,0 +1,5 @@
+// Script to download files from zoltar
+
+const fs = require('fs')
+const mmwr = require('mmwr-week')
+const zoltar = require('./zoltar')
---
+++ zoltar/zoltar.js
@@ -0,0 +1,28 @@
+// Module for interacting with zoltar
+
+const rp = require('request-promise')
+
+
+class Zoltar {
+  constructor (zoltarRoot) {}
+
+  get projects () {}
+}
+
+class ZProject {
+  constuctor (projectData) {}
+
+  get models () {}
+}
+
+class ZModel {
+  constructor (modelData) {}
+
+  get forecasts () {}
+}
+
+class ZForecast {
+  constructor (forecastData) {}
+
+  get csv () {}
+}
